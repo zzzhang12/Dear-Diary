8d215df37270e03fc412e09252a64dbad85ca787
zzzhang12 <zhangz_12@outlook.com>
2025-04-26 01:24:29 +0100

    feat: add views and urls to display html templates

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "entries/index.html")
+
+
+def add(request):
+    return render(request, "entries/add.html")
