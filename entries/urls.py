bf731f6f426ff7003243fd6d0044b7e363e994e9
zzzhang12 <zhangz_12@outlook.com>
2025-04-26 01:28:46 +0100

    feat: add a link to the "add" page

--- entries/urls.py
+++ entries/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index), path("add/", views.add)]
+urlpatterns = [path("", views.index, name="home"), path("add/", views.add, name="add")]
